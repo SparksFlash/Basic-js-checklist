45e12ca43d48079e968b8646a0bd1977906ecc60
SparksFlash <ug2102056@cse.pstu.ac.bd>
2024-04-21 18:37:21 +0600

    problem-solving

--- problem-solving-checklist.js
+++ problem-solving-checklist.js
@@ -1,19 +1,31 @@
 // 1. conversation
-feetToInch
+//feetToInch
+
+function feetToInches(feet){
+    return feet * 12;
+  }
+  const inch = feetToInches(14);
+  console.log(inch);
 
 // 2. conversion
-centimeterToMeter
+//centimeterToMeter
+
+function centimeterToMeter(centimeter){
+    return centimeter / 100;
+}
+const meter = centimeterToMeter(14);
+console.log(meter);
 
 
 // 3. calculation
-pageRequirements
+//pageRequirements
 
 // book1 has 100 pages
 // book2 has 200 pages
 // book3 has 300 pages
 
 //4. friends
-bestFriend
+//bestFriend
 
 //5. will stop the loop if the array has any negative number and returns all the positive number before the negative number
-onlyPositive
+//onlyPositive
